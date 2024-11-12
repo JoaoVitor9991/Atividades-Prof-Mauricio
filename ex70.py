ec2686dfa92694fb80b546e5f8be17e3d3241c5f
JoaoVitor9991 <joaovitormachadodev99@gmail.com>
2024-11-11 21:44:19 -0400

    ultimos ex lista 1

---
+++ ex70.py
@@ -0,0 +1,7 @@
+def classifica_triangulo(a, b, c):
+    if a == b == c:
+        return "Equilátero"
+    elif a == b or a == c or b == c:
+        return "Isósceles"
+    else:
+        return "Escaleno"
